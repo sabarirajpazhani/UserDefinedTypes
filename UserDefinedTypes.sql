1147b9a13e909f3b00f04f24bc11a6662ec72619
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-09 11:58:40 +0530

    User Defined data types

--- UserDefinedTypes.sql
+++ UserDefinedTypes.sql
@@ -4,5 +4,8 @@ create database UserDefinedTypes;
 --User Database
 Use UserDefinedTypes;
 
+--create Database for Phone number and EmailID 
+create type PhoneType from varchar(20) not null; --For Phone Number
+create type EmailType from varchar(90) not null; --for Email
 
 
