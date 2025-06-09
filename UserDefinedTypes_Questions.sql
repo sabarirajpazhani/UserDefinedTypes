be50c0334c1c238047baa0c575be73bdb09cbbff
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-09 13:49:26 +0530

    Add files via upload

---
+++ UserDefinedTypes_Questions.sql
@@ -0,0 +1,21 @@
+--Part 1: User-Defined Data Type (UDDT) – Practice Questions 
+/* Q1: Create a table named Customers with the following columns:
+CustomerID (INT, Primary Key)
+
+CustomerName (VARCHAR)
+
+CustomerEmail (use EmailType)
+
+CustomerPhone (use PhoneType)*/
+
+--creating the user defined data type for Email and Phone 
+create type PhoneType from varchar(20) not null;
+create type EmailType from varchar(90) not null;
+
+create table Customers(
+	CustomerID int Primary key,
+	CustomerName varchar(80),
+	CustomerEmail EmailType,
+	CustomerPhone PhoneType
+);
+
